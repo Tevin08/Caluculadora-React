581650782c585e90b91bd499896302da9534a415
tevin08 <estevaopsg08@gmail.com>
2023-11-25 11:18:21 -0300

    stylist complete

--- Calculadora/src/App.jsx
+++ Calculadora/src/App.jsx
@@ -1,38 +1,51 @@
 import { useState } from 'react';
 import Button from './assets/components/Buttons';
-import {  DiscordLogo, Eraser, Rocket, Trash} from '@phosphor-icons/react'
+import { DiscordLogo, Eraser, Rocket, Trash } from '@phosphor-icons/react'
 import { YoutubeLogo } from '@phosphor-icons/react/dist/ssr';
 
 function App() {
- const style1= {
-   backgroundColor : '#D91E36'
- } // exemplo de prps muito diferentes
+  const style1 = {
+    backgroundColor: '#D91E36'
+  }
+  const style2 = {
+    backgroundColor: '#828792'
+    //#C42348 cardinals
+  }
+
+  const style3 = {
+    backgroundColor: '#06D6A0'
+  }
+  // exemplo de prps muito diferentes
+
+  const [val, setval] = useState('')
 
   return (
     <main className='flex h-screen w-screen justify-center items-center  bg-blue-800'>
-      <div className='flex flex-col gap-6  bg-zinc-800 w-1/2 max-w-sm  '>
-        <div className='py-6 px-8 w-full    bg-blue-500  '/>  
+      <div className='flex flex-col   bg-zinc-800 w-1/2 max-w-sm  '>
+        <div className={`py-6 px-8 w-full  text-2xl text-slate-400 text-right     bg-zinc-800  `} >
+          {val.toString()}
+        </div>
         <div className="grid grid-cols-3 p-6 justify-items-center gap-4 ">
-       
-       
-        <Button icon={ <Trash/>}  />
-        <Button icon={ <Eraser/>} style={style1}  />
-        <Button icon={ <Trash/>}  />
-        <Button number={0}/>
-        <Button number={1}/>
-        <Button number={'+'}  blue={true} />
-        <Button number={2}/>
-        <Button number={3}/>
-        <Button number={'-'} blue={true}  />
-        <Button number={4}/>
-        <Button number={5}/>
-        <Button number={'×'}   blue={true} />
-        <Button number={6}/>
-        <Button number={7}/>
-        <Button number={'÷'}  blue={true} />
-        <Button number={8}/>
-        <Button number={9}   />  
-        <Button number={'='}   blue={true} />
+
+
+          <Button icon={','} />
+          <Button icon={<Eraser color='#' />} style={style2} />
+          <Button icon={<Trash />} style={style1} />
+          <Button number={0} onclick={setval} />
+          <Button number={1} onclick={setval} />
+          <Button number={'+'} blue={true} />
+          <Button number={2} onclick={setval} />
+          <Button number={3} onclick={setval} />
+          <Button number={'-'} blue={true} />
+          <Button number={4} onclick={setval} />
+          <Button number={5} onclick={setval} />
+          <Button number={'×'} blue={true} />
+          <Button number={6} onclick={setval} />
+          <Button number={7} onclick={setval} />
+          <Button number={'÷'} blue={true} />
+          <Button number={8} onclick={setval} />
+          <Button number={9} onclick={setval} />
+          <Button number={'='} blue={true} />
         </div>
       </div>
     </main>
--- Calculadora/src/assets/components/Buttons.jsx
+++ Calculadora/src/assets/components/Buttons.jsx
@@ -1,12 +1,13 @@
 import { Trash } from "@phosphor-icons/react";
 
-export default function Button({number,blue, style,icon}){
+export default function Button({number,blue, style,icon,onclick}){
     
     return(
         
             <button 
-                className={`flex items-center justify-center   ${blue ? 'bg-sky-400':'bg-zinc-600'}  w-full h-12`  } 
+                className={`flex items-center justify-center  text-2xl  ${blue ? 'bg-sky-400':'bg-zinc-600'}  w-full h-12`  } 
                 style={style}
+                onClick = {()=> onclick(number)}
             > 
                 {number}
                 {icon}
