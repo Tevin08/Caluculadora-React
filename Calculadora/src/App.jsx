feeffeed12e018b995217b2d0a4a8f28cc597ff8
estevao <estevaopsg08@gmail.com>
2023-11-27 14:54:58 -0300

    update

--- Calculadora/src/App.jsx
+++ Calculadora/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from './assets/components/Buttons';
 import { DiscordLogo, Eraser, Rocket, Trash } from '@phosphor-icons/react'
 import { YoutubeLogo } from '@phosphor-icons/react/dist/ssr';
@@ -18,34 +18,36 @@ function App() {
   // exemplo de prps muito diferentes
 
   const [val, setval] = useState('')
-
+useEffect(()=> {
+console.log(val)
+},[val])
   return (
     <main className='flex h-screen w-screen justify-center items-center  bg-blue-800'>
-      <div className='flex flex-col   bg-zinc-800 w-1/2 max-w-sm  '>
-        <div className={`py-6 px-8 w-full  text-5xl text-slate-400 text-right     bg-zinc-800  `} >
+      <div className='flex flex-col   bg-zinc-800 w-1/2  rounded-xl max-w-sm  '>
+        <div className={`py-6 px-8 w-full  text-5xl text-slate-400 break-words text-right `} >
           {val}
         </div>
-        <div className="grid grid-cols-3 p-6 justify-items-center gap-4 ">
+        <div className="grid grid-cols-3 p-6 justify-items-center  gap-4 ">
 
 
-          <Button icon={','} onclick={setval} />
-          <Button icon={<Eraser color='#' />} style={style2} />
-          <Button icon={<Trash />} style={style1} />
-          <Button number={0} onclick={setval} />
-          <Button number={1} onclick={setval} />
-          <Button number={'+'} blue={true} />
-          <Button number={2} onclick={setval} />
-          <Button number={3} onclick={setval} />
-          <Button number={'-'} blue={true} />
-          <Button number={4} onclick={setval} />
-          <Button number={5} onclick={setval} />
-          <Button number={'×'} blue={true} />
-          <Button number={6} onclick={setval} />
-          <Button number={7} onclick={setval} />
-          <Button number={'÷'} blue={true} />
-          <Button number={8} onclick={setval} />
-          <Button number={9} onclick={setval} />
-          <Button number={'='} blue={true} />
+          <Button number={','} onclick={setval} value={val} />
+          <Button icon={<Eraser color='#d6d6d6' />} style={style2} />
+          <Button icon={<Trash color='#d6d6d6' />} style={style1} />
+          <Button number={0} onclick={setval} value={val} />
+          <Button number={1} onclick={setval} value={val} />
+          <Button number={'+'} blue={true} onclick={setval} value={val}/>
+          <Button number={2} onclick={setval} value={val} />
+          <Button number={3} onclick={setval} value={val} />
+          <Button number={'-'} blue={true} onclick={setval} value={val}/>
+          <Button number={4} onclick={setval} value={val} />
+          <Button number={5} onclick={setval} value={val} />
+          <Button number={'×'} blue={true} onclick={setval} value={val}/>
+          <Button number={6} onclick={setval} value={val} />
+          <Button number={7} onclick={setval} value={val} />
+          <Button number={'÷'} blue={true} onclick={setval} value={val}/>
+          <Button number={8} onclick={setval} value={val} />
+          <Button number={9} onclick={setval} value={val} />
+          <Button number={'='} blue={true} onclick={setval} value={val}/>
         </div>
       </div>
     </main>
