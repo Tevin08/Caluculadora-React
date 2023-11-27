705e226e0f0bc14c9d4cc2ad06b97657d1806f95
estevao <estevaopsg08@gmail.com>
2023-11-27 15:34:56 -0300

    quase

--- Calculadora/src/App.jsx
+++ Calculadora/src/App.jsx
@@ -18,42 +18,46 @@ function App() {
   // exemplo de prps muito diferentes
 
   const [val, setval] = useState('')
-
-  function Conta ()  {
-    
+  
+  function Conta() {
+    const resultado = eval(val)
+    console.log(resultado);
+      return resultado
   }
 
-useEffect(()=> {
-console.log(val)
-},[val])
+  useEffect(() => {
+    console.log(val)
+  }, [val])
   return (
     <main className='flex h-screen w-screen justify-center items-center  bg-blue-800'>
       <div className='flex flex-col   bg-zinc-800 w-1/2  rounded-xl max-w-sm  '>
         <div className={`py-6 px-8 w-full  text-5xl text-slate-400 break-words text-right `} >
-          {val}
-          
+if () {
+  
+}
+        {val}
         </div>
         <div className="grid grid-cols-3 p-6 justify-items-center  gap-4 ">
 
 
-          <Button number={','} onclick={setval} value={val} />
+          <Button number={'.'} onclick={setval} value={val} />
           <Button icon={<Eraser color='#d6d6d6' />} style={style2} />
           <Button icon={<Trash color='#d6d6d6' />} style={style1} />
           <Button number={0} onclick={setval} value={val} />
           <Button number={1} onclick={setval} value={val} />
-          <Button number={'+'} blue={true} onclick={setval} value={val}/>
+          <Button number={'+'} blue={true} onclick={setval} value={val} />
           <Button number={2} onclick={setval} value={val} />
           <Button number={3} onclick={setval} value={val} />
-          <Button number={'-'} blue={true} onclick={setval} value={val}/>
+          <Button number={'-'} blue={true} onclick={setval} value={val} />
           <Button number={4} onclick={setval} value={val} />
           <Button number={5} onclick={setval} value={val} />
-          <Button number={'*'} blue={true} onclick={setval} value={val}/>
+          <Button number={'*'} blue={true} onclick={setval} value={val} />
           <Button number={6} onclick={setval} value={val} />
           <Button number={7} onclick={setval} value={val} />
-          <Button number={'/'} blue={true} onclick={setval} value={val}/>
+          <Button number={'/'} blue={true} onclick={setval} value={val} />
           <Button number={8} onclick={setval} value={val} />
           <Button number={9} onclick={setval} value={val} />
-          <Button number={'='} blue={true} onclick={setval} value={val}/>
+          <Button number={'='} blue={true} onclick={Conta}  value={val} />
         </div>
       </div>
     </main>
