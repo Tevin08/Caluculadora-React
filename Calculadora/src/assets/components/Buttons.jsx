9576711f79ba25cba59855f54dd8b5cf77973e4e
estevao <estevaopsg08@gmail.com>
2023-11-27 14:53:11 -0300

    funcionando pouco

--- Calculadora/src/assets/components/Buttons.jsx
+++ Calculadora/src/assets/components/Buttons.jsx
@@ -1,13 +1,13 @@
 import { Trash } from "@phosphor-icons/react";
 
-export default function Button({number,blue, style,icon,onclick}){
+export default function Button({number,blue, style,icon,onclick, value}){
 
     return(
         
             <button 
-                className={`flex items-center justify-center  text-2xl rounded-full ${blue ? 'bg-sky-400':'bg-zinc-600'}  w-full h-12`  } 
+                className={`flex items-center justify-center  text-2xl rounded-full ${blue ? 'bg-sky-400  text-slate-50'   :'bg-zinc-600'}  w-full h-12 text-slate-400 `  } 
                 style={style}
-                onClick = {()=> onclick(number ? number: number.toString())}
+                onClick = {()=> onclick(`${value}${number}`)}     
             > 
                 {number}
                 {icon}
